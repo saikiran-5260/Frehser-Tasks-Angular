52481d53a96f2324cfc3e2eee86560366ff0362a
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-01 13:01:39 +0530

    Update tasks.component.ts

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';  
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -10,5 +10,16 @@ import { Task } from 'src/app/Task';
 export class TasksComponent {
 
   tasks: Task[] = [];
+
+  constructor(private taskService: TaskService){}
+
+  ngOnInit(): void{
+   this.taskService.getTasks().subscribe((tasks)=>this.tasks = tasks);
+
+  }
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task).subscribe(()=>this.tasks = this.
+    tasks.filter(t=>t.id! !== task.id));
+  }
 
 }
